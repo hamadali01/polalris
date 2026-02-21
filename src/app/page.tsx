ed31cce2a3ec70689443f4b6292d8ea3ea0ccbf5
Haider Ali <haiderali@Haiders-MacBook-Pro.local>
2026-02-22 03:16:16 +0500

    added projects list in app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,9 @@
-"use client";
+import { ProjectsView } from "@/features/projects/components/projects-view"
 
-import { useMutation, useQuery } from "convex/react";
-import { api } from "../../convex/_generated/api"
-import { Button } from "@/components/ui/button";
-
-const Page = () => {
-  const projects = useQuery(api.projects.get)
-  const createProject = useMutation(api.projects.create)
+const Home = () => {
   return (
-    <div className="flex flex-col gap-2 p-4">
-      <Button onClick={() => createProject({
-        name: "Project"
-      })}>
-        Add New
-      </Button>
-      {projects?.map((project) => (
-        <div className="flex flex-col border rounded p-2" key={project._id}>
-          <h3>{project.name}</h3>
-          <p>Owner ID: {project.ownerId}</p>
-        </div>
-      ))}
-    </div>
+    <ProjectsView />
   )
 }
 
-export default Page;
+export default Home;
